fc29c330c827f5bc776df1fc7d4c6055bd713c6b
naveensakthij <naveensakthijega@email.com>
2026-06-21 22:41:15 +0530

    updated yml

--- tests/test_kindle_login.py
+++ tests/test_kindle_login.py
@@ -1,3 +1,8 @@
+import sys
+from pathlib import Path
+project_root = Path(__file__).parent.parent
+sys.path.insert(0, str(project_root))
+
 import os
 from pages.kindle_login_page import KindleLoginPage
 from dotenv import load_dotenv
